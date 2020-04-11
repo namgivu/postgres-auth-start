1ca51405f10a1455be8051eef082601ecfceef54
namgivu <namgivu@gmail.com>
2020-04-11 18:31:47 +0700

    psql server 0th

---
+++ 00.postgres-server.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+export v='postgres_10'  # v aka version
+
+echo '--> ENSURE we have postgres instance working'
+    docker ps | grep -E "nn.+$v|IMAGE" --color=always; echo
+    o='
+    CONTAINER ID        IMAGE                COMMAND                  CREATED             STATUS              PORTS                     NAMES
+    xxxxxxxxxxxx        postgres:10          "docker-entrypoint.s…"   28 minutes ago      Up 28 minutes       0.0.0.0:20410->5432/tcp   nn_postgres__postgres_10
+    '
+
+    # nail the :c :p of the instance; c aka container, p aka port
+    export c=`docker ps --format '{{.Names}}' | grep -E "nn.+$v" | head -n1 `; echo $c  # c aka container
+    export p=`docker ps --format '{{.Names}} {{.Ports}}' | grep -E "$c" | cut -d' ' -f2 | cut -d'-' -f1 | cut -d':' -f2 `; echo $p  # p aka port
+
+    # nail the :user :pass of the instance
+    usr=`docker exec -it $c bash -c 'echo $POSTGRES_USER' `     ; usr=${usr::-1}; echo "usr=$usr"  # remove LF as ending character ref. https://stackoverflow.com/a/27658733/248616
+    psw=`docker exec -it $c bash -c 'echo $POSTGRES_PASSWORD' ` ; psw=${psw::-1}; echo "psw=$psw"  # remove LF as ending character ref. https://stackoverflow.com/a/27658733/248616
+
+
+echo; echo "--> SUMMARY
+psql://$usr:$psw@$c:$p
+          c=$c
+          p=$p
+    usr/psw=$usr/$psw
+"
+    printf 'Check if-responding port :p ... '
+    echo 'QUIT' | nc -w 1 localhost $p && echo 'PASS' || echo 'FAIL'  # check responding port ref. https://gist.github.com/namgivu/414b71b4a4c894dcf01494f4f4c225d1
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-We will use postgres instance created with docker as detailed [here](https://github.com/namgivu/postgres-docker)
+Study how postgres authentication work.
+ 
+`00` Prepare postgres server instance ref. `00.postgres-server.sh`
+**TODO** `01` Prepare postgres client to get authenticated against `00` ref. `01.postgres-client.sh`
+
+
+# Prepare postgres server
+Postgres instance created with docker as detailed [here](https://github.com/namgivu/postgres-docker)
+Starting with PostgreSQL 10 
+
+
+**TODO**
+printf 'Check if-working psql instance ... '
+psql -U postgres -h localhost -p $p -c 'select 1' -t && echo 'PASS' || echo 'FAIL'
